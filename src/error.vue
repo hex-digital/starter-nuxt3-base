ae3c1f7c71c0ad287c7cca10218f82f191500a7c
Jamie Warburton <jamie@hexdigital.com>
2022-12-20 14:03:29 +0000

    feat: add custom error page template

---
+++ src/error.vue
@@ -0,0 +1,19 @@
+<script setup>
+const error = useError();
+
+function handleError() {
+  clearError({
+    redirect: '/',
+  });
+}
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <h1>{{ error?.statusCode || 'Error' }}</h1>
+      <p>Something went wrong! Please refresh and try again.</p>
+      <a class="u-underline u-cursor-pointer" @click="handleError">Go to the homepage</a>
+    </NuxtLayout>
+  </div>
+</template>
